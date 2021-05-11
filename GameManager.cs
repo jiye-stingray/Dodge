00d54a02a70ab0e390ef50544884845c43a19dec
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-05-11 15:17:08 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UI 관련 라이브러리
+using UnityEngine.SceneManagement; //씬 관련 라이브러리
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText; //게임오버 시 활성화할 텍스트 게임 오브젝트
+    public Text timeText; //생존 시간
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
